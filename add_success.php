8bb3e3a5f06c886782270a53984a0f48da6bf4cf
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2022-09-29 14:59:28 +1300

    creating validation for entry form

---
+++ add_success.php
@@ -0,0 +1,14 @@
+<?php include("topbit.php"); ?>
+                       
+            
+        <div class="box main">
+            <h2>Congratulations</h2>
+            
+            
+            <p>
+                you added a thing
+            </p>
+
+        </div> <!-- / main -->
+
+<?php include("bottombit.php"); ?>
--- addentry.php
+++ addentry.php
@@ -1,11 +1,5 @@
 <?php include("topbit.php"); 
 
-$type_sql = "SELECT * FROM `pokemon_type` ORDER BY `TypeID` ASC
-";
-$type_query = mysqli_query($dbconnect, $type_sql);
-$type_rs = mysqli_fetch_assoc($type_query);
-
-
 $pokemon_name = "";
 $poke_type_1 = "";
 $poke_type_2 = "";
@@ -19,6 +13,79 @@ $speed = "";
 $generation = "";
 $legendary = "";
 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $pokemon_name = mysqli_real_escape_string($dbconnect, $_POST['pokemon_name']);
+
+    if ($pokemon_name == ""){
+        $has_errors = "yes";
+    }
+
+    $poke_type_1 = mysqli_real_escape_string($dbconnect, $_POST['poke_type_1']);
+    if ($poke_type_1 == ""){
+        $has_errors == "yes";
+    }
+
+    $poke_type_2 = mysqli_real_escape_string($dbconnect, $_POST['poke_type_2']);
+    if ($poke_type_2 == ""){
+        $has_errors == "yes";
+    }
+    
+    $total = mysqli_real_escape_string($dbconnect, $_POST['total']);
+    
+    if ($total == ""){
+        $has_errors == "yes";
+    }
+
+    $hp = mysqli_real_escape_string($dbconnect, $_POST['hp']);
+    
+    if ($hp == ""){
+        $has_errors == "yes";
+    }
+
+    $attack = mysqli_real_escape_string($dbconnect, $_POST['attack']);
+    if ($attack == ""){
+        $has_errors == "yes";
+    }
+
+    $defense = mysqli_real_escape_string($dbconnect, $_POST['defense']);
+    if ($defense == ""){
+        $has_errors == "yes";
+    }
+    
+    $spatk = mysqli_real_escape_string($dbconnect, $_POST['spatk']);
+    if ($spatk == ""){
+        $has_errors == "yes";
+    }
+
+    $spdef = mysqli_real_escape_string($dbconnect, $_POST['spdef']);
+    if ($spdef == ""){
+        $has_errors == "yes";
+    }
+
+    $speed = mysqli_real_escape_string($dbconnect, $_POST['speed']);
+    if ($speed == ""){
+        $has_errors == "yes";
+    }
+    
+    $generation = mysqli_real_escape_string($dbconnect, $_POST['generation']);
+    if ($generation == ""){
+        $has_errors == "yes";
+    }
+    
+    $legendary = mysqli_real_escape_string($dbconnect, $_POST['legendary']);
+    if ($legendary == ""){
+        $has_errors == "yes";
+    }
+
+    if ($has_errors != "yes"){
+    // go to sucecss page
+    header('Location: add_success.php');
+    }
+
+}
+
 ?>
                        
             
@@ -29,67 +96,86 @@ $legendary = "";
             <form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
             
             <!-- Pokemon Name -->
-            <input class="add-field" type="text" name="pokemon_name" placeholder="Pokemon Name..."/>
+            <input class="add-field" type="text" name="pokemon_name" placeholder="Pokemon Name..." required/>
                 
             <!-- Pokemon Type 1 -->
-            <select class="adv" name="poke_type_1">
-            <option value="" selected>Pokemon Type 1...</option>
+            <select class="adv" name="poke_type_1" required>
+            <option value="" selected disabled>Pokemon Type 1...</option>
 
             <!-- get options from database -->
 
             <?php 
+
+            $type_sql_1 = "SELECT * FROM `pokemon_type` ORDER BY `TypeID` ASC
+            ";
+            $type_query_1 = mysqli_query($dbconnect, $type_sql_1);
+            $type_rs_1 = mysqli_fetch_assoc($type_query_1);
+
+
             do {
             ?>
-            <option value="<?php echo $type_rs['TypeID']; ?>"></option>
+            <option value="<?php echo $type_rs_1['TypeID']; ?>"><?php echo $type_rs_1['Type']; ?></option>
 
             <?php
             } // end type do loop
 
-            while ($type_rs = mysqli_fetch_assoc($type_query))
+            while ($type_rs_1 = mysqli_fetch_assoc($type_query_1))
             ?>
 
             </select>
 
             <!-- Pokemon Type 2 -->
-            <select class="adv" name="poke_type_2">
-            <option value="" selected>Pokemon Type 2...</option>
+            <select class="adv" name="poke_type_2" required>
+            <option value="" selected disabled>Pokemon Type 2...</option>
 
             <!-- get options from database -->
 
             <?php 
+
+            $type_sql_2 = "SELECT * FROM `pokemon_type` ORDER BY `TypeID` ASC
+            ";
+            $type_query_2 = mysqli_query($dbconnect, $type_sql_2);
+            $type_rs_2 = mysqli_fetch_assoc($type_query_2);
+
             do {
             ?>
-            <option value="<?php echo $type_rs['TypeID']; ?>"></option>
+            <option value="<?php echo $type_rs_2['TypeID']; ?>"><?php echo $type_rs_2['Type']; ?></option>
 
             <?php
             } // end type do loop
 
-            while ($type_rs = mysqli_fetch_assoc($type_query))
+            while ($type_rs_2 = mysqli_fetch_assoc($type_query_2))
             ?>
 
             </select>
 
-            <input class="add-field" type="text" name="total" placeholder="Total..." />
+            <input class="add-field" type="text" name="hp" placeholder="HP..." required/>
 
-            <input class="add-field" type="text" name="hp" placeholder="HP..." />
+            <input class="add-field" type="text" name="attack" placeholder="Attack..." required/>
 
-            <input class="add-field" type="text" name="attack" placeholder="Attack..." />
+            <input class="add-field" type="text" name="defense" placeholder="Defense..." required/>
 
-            <input class="add-field" type="text" name="defense" placeholder="Defense..." />
+            <input class="add-field" type="text" name="spatk" placeholder="Special Attack..." required/>
 
-            <input class="add-field" type="text" name="spatk" placeholder="Special Attack..." />
+            <input class="add-field" type="text" name="spdef" placeholder="Special Defense..." required/>
 
-            <input class="add-field" type="text" name="spdef" placeholder="Special Defense..." />
+            <input class="add-field" type="text" name="speed" placeholder="Speed..." required/>
 
-            <input class="add-field" type="text" name="speed" placeholder="Speed..." />
-
-            <input class="add-field" type="text" name="gen" placeholder="Generation..." />
+            <input class="add-field" type="text" name="generation" placeholder="Generation..." required/>
+            
+            <br />
+            <br />
 
             <!-- defaults to 'no' -->
             <!-- NOTE: value in database is boolean, 1 is yes and 0 is no -->
             <b>Legendary: </b>
             <input type="radio" name="legendary" value="1"/>Yes
             <input type="radio" name="legendary" value="0" checked="checked"/>No
+
+            <br />
+            <br />
+
+            <input class="submit advanced-button" type="submit" value="Submit" />
 
             </form>
 
